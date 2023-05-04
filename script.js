6e34ef67b5a4de396261d43041139aef80d52e78
nelly tumukunde <nelly.tumukunde@aionem.net>
2023-05-04 22:18:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const formOpenBtn = document.querySelector("#open-btn"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form-container"),
+formClose = document.querySelector(".form-close"),
